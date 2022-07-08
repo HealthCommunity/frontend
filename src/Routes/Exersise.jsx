8d88fff1678c6cf91fb9a18b170afc3decef7b1b
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-07-08 18:17:20 +0900

    Feat : 게시판 Banner 추가

--- src/Routes/Exersise.jsx
+++ src/Routes/Exersise.jsx
@@ -5,13 +5,15 @@ import { ex } from "../FakeD/exersise";
 import { RightDiv } from "../components/Main/HomeSeparate";
 import BoardNav from "../components/Board/BoardNavigation/BoardNav";
 import Footer from '../components/Footer/Footer'
+import ExersiseSlider from "../components/Board/BoardSlider/ExersiseSlider";
 
 function Exersise(){
     return (
         <>
             <Title name="Exersise"/>
             <Nav/>
-            <BoardNav style={{paddingTop:"150px" ,width:"50%" , margin:"0 auto",pointerEvents:"none"}} name={"운동 게시판"} summary={" 다른 사람의 운동 경험을 확인해보세요!"}/>
+            <ExersiseSlider/>
+            <BoardNav style={{width:"50%" , margin:"0 auto",pointerEvents:"none"}} name={"운동 게시판"} summary={" 다른 사람의 운동 경험을 확인해보세요!"}/>
             <RightDiv style={{marginBottom:"150px", paddingRight:"0px"}}>
                 {ex.map((x)=>
                     <SoloBoardItem key={Math.random()} item={x} color={"#7FBA7A"} value={"exersise"}/>
--- src/components/Board/BoardSlider/BoardSlider.jsx
+++ src/components/Board/BoardSlider/BoardSlider.jsx
@@ -1,81 +1,12 @@
 
-import styled from 'styled-components';
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper";
-import { Link } from 'react-router-dom';
+import BoardSliderItem from "./BoardSliderItem";
 
-const BannerDiv = styled.div`
-    width:500px;
-    height:100px;
-    border-radius: ${(props)=>props.theme.radiusSize16};
-    padding:10px;
-    display: flex;
-    background-color: ${(props)=>props.theme.colorPointBlue200};
-    box-shadow: ${(props)=>props.theme.shadowColor2};
-    @media all and (min-width:480px) and (max-width:767px) {
-        width:700px;
-    } 
-    @media all and (max-width:479px) {
-        width:700px;
-    }
-`
 
-const BannerLeft = styled.div`
-    width: 40%;
-    display: flex;
-    font-size:${(props)=>props.theme.fontSizeH3};
-    font-weight: 600;
-    justify-content: center;
-    align-items: center;
-    :hover{
-        transform: scale(1.1);
-        transition-duration: 1s;
-    }
-    @media all and (min-width:480px) and (max-width:767px) {
-        font-size:${(props)=>props.theme.fontSizeH4};
-        margin-right: 10px;
-    } 
-    @media all and (max-width:479px) {
-        font-size:${(props)=>props.theme.fontSizeH4};
-        margin-right: 10px;
-    }
-    
-`
-
-const BannerRight= styled.div`
-    width:60%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-`
-const BannerRightItemTitle= styled.div`
-    font-size:${(props)=>props.theme.fontSizeH4};
-    font-weight: 500;
-    margin-bottom: 10px;
-    @media all and (min-width:480px) and (max-width:767px) {
-        font-size:${(props)=>props.theme.fontSizeH6};
-        
-    } 
-    @media all and (max-width:479px) {
-        font-size:${(props)=>props.theme.fontSizeH6};
-    }
-`
-
-const BannerRightItem =styled.div`
-    font-size:${(props)=>props.theme.fontSizeH6};
-    line-height: 1.5em;
-    @media all and (min-width:480px) and (max-width:767px) {
-        font-size:${(props)=>props.theme.fontSizeH7};
-        
-    } 
-    @media all and (max-width:479px) {
-        font-size:${(props)=>props.theme.fontSizeH7};
-    }
-`
 
 function BoardSlider ( ){
     return (
@@ -93,61 +24,20 @@ function BoardSlider ( ){
         modules={[Autoplay, Pagination, Navigation]}
         style={{  width: "70%",height: "200px" , paddingTop:"100px", textAlign:"center" , fontSize:"20px"}}
       >
-        <SwiperSlide style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-            <BannerDiv>
-                    <BannerLeft>
-                        <a href={"http://speedandpower.co.kr/"} target="_blank" rel="noreferrer">삽짐</a>
-                    </BannerLeft>
-                <BannerRight>
-                    <BannerRightItemTitle>운동 정보</BannerRightItemTitle>
-                    <BannerRightItem>
-                        크로스핏이나 파워트레이닝 운동정보 찾는 곳
-                    </BannerRightItem>
-                </BannerRight>
-            </BannerDiv>
+         <SwiperSlide style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+           <BoardSliderItem color={"#3F8CFF"} url={"http://speedandpower.co.kr/"} urlname={"삽짐"} title={"운동 정보"} summary={'크로스핏이나 파워트레이닝 등 운동 정보를 찾는 곳'}/>
         </SwiperSlide>
         <SwiperSlide style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-            <BannerDiv>
-                <BannerLeft>
-                    <a href={"https://www.ggjil.com/schedule.php"} target="_blank">
-                    개근질닷컴</a></BannerLeft>
-                <BannerRight>
-                    <BannerRightItemTitle>보디빌링 관련 사이트</BannerRightItemTitle>
-                    <BannerRightItem>대회일정 , 운동 정보에 대해 확인해 볼 수 있다</BannerRightItem>
-                </BannerRight>
-            </BannerDiv>
+            <BoardSliderItem color={"#3F8CFF"} url={"https://www.ggjil.com/schedule.php"} urlname={"개근질닷컴"} title={"보디빌딩 관련 사이트"} summary={'보디빌딩 대회일정 , 운동 정보에 대해 확인해 볼 수 있다'}/>
         </SwiperSlide>
         <SwiperSlide style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-            <BannerDiv>
-                <BannerLeft>
-                    <a href= {"https://www.bodybuilding.com/"} target="_blank" rel="noreferrer">
-                    보디빌링 닷컴</a></BannerLeft>
-                <BannerRight>
-                    <BannerRightItemTitle>웨이트 트레이닝 관련 정보 사이트</BannerRightItemTitle>
-                    <BannerRightItem>각 카테고리마다 상품, 운동정보를 확인해 볼 수 있습니다</BannerRightItem>
-                </BannerRight>
-            </BannerDiv>
+            <BoardSliderItem color={"#3F8CFF"} url={"https://www.bodybuilding.com/"} urlname={"보디빌딩 닷컴"} title={"웨이트 트레이닝 관련 정보 사이트"} summary={'각 카테고리마다 상품, 운동정보를 확인해 볼 수 있습니다'}/>
         </SwiperSlide>
         <SwiperSlide style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-            <BannerDiv>
-                <BannerLeft>
-                    <a href = {"https://glycemicindex.com/"} target="_blank" rel="noreferrer">
-                    글리세믹 인덱스</a></BannerLeft>
-                <BannerRight>
-                    <BannerRightItemTitle>식품 정보</BannerRightItemTitle>
-                    <BannerRightItem>식품 품종, 상태, 조리법에 대한 정보가 있습니다</BannerRightItem>
-                </BannerRight>
-            </BannerDiv>
+            <BoardSliderItem color={"#3F8CFF"} url={"https://glycemicindex.com/"} urlname={"글리세믹 인덱스"} title={"식품정보"} summary={'식품 품종,상태,조리법에 대한 정보가 있습니다'}/>
         </SwiperSlide>
         <SwiperSlide style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-            <BannerDiv>
-                <BannerLeft>
-                    <a href={"http://koreanfood.rda.go.kr/kfi/fct/fctFoodComp/list"} target="_blank" rel="noreferrer"> 농촌진흥청</a></BannerLeft>
-                <BannerRight>
-                    <BannerRightItemTitle>식품 영양 정보</BannerRightItemTitle>
-                    <BannerRightItem>열량, 단백질, 탄수화물, 지방 등 성분을 찾아볼 수 있습니다</BannerRightItem>
-                </BannerRight>
-            </BannerDiv>
+            <BoardSliderItem color={"#3F8CFF"} url={"http://koreanfood.rda.go.kr/kfi/fct/fctFoodComp/list"} urlname={"농촌진흥청"} title={"식품 영양 정보"} summary={'열량, 단백질, 탄수화물, 지방 등 성분을 찾아볼 수 있습니다'}/>
         </SwiperSlide>
       </Swiper>
     </>
