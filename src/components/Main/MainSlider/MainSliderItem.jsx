a2cafaaca77c92fb1b77acc202d5732d9943bc33
pasong0511 <pasong0511@gmail.com>
2022-07-09 17:21:03 +0900

    Feat : 홈화면 main slider 자동 슬라이드 구현

---
+++ src/components/Main/MainSlider/MainSliderItem.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const SliderItem = styled.div`
+    width: 172px;
+    height: 225px;
+    border-radius: 20px;
+    /* display: flex;
+    justify-content: center;
+    align-items: center; */
+
+    img {
+        width: 100%;
+        height: 100%;
+        border-radius: 14px;
+    }
+
+    &.active {
+        img {
+            width: 200%;
+            height: 200%;
+            border-radius: 14px 0 0 14px;
+        }
+    }
+`;
+
+function MainSliderItem({ title, urlimage, active = false }) {
+    return (
+        <SliderItem className={active ? "active" : ""}>
+            <img alt="" src={`${urlimage}`} />
+        </SliderItem>
+    );
+}
+
+export default MainSliderItem;
