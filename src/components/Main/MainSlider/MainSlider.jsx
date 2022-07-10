c9b64f089008f6c6dd11b76b75260415e970d4db
pasong0511 <pasong0511@gmail.com>
2022-07-11 00:46:20 +0900

    Feat : main 슬라이더 수정 중

--- src/components/Main/MainSlider/MainSlider.jsx
+++ src/components/Main/MainSlider/MainSlider.jsx
@@ -5,6 +5,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Autoplay, Pagination, Navigation } from "swiper";
+
 import MainSliderItem from "./MainSliderItem";
 
 import { popular } from "../../../FakeD/popular";
@@ -20,61 +21,82 @@ const MainSliderDiv = styled.div`
     /* background-color: ${(props) => props.theme.navBgColor}; */
 `;
 
+const Temp = styled.div`
+    border-radius: 8px;
+    position: relative;
+    width: 100%;
+    height: 100%;
+
+    & .swiper-slide {
+        width: 172px;
+    }
+
+    & .swiper-slide.active {
+        width: 640px;
+
+        /* @media all and (min-width: 480px) and (max-width: 767px) {
+            width: calc(100% - 20px);
+        }
+        @media all and (max-width: 479px) {
+            width: calc(100% - 20px);
+        } */
+    }
+`;
+
 function MainSlider() {
-    //console.log(popular);
     const items = popular;
     const [active, setActive] = useState(0);
 
-    //console.log(items);
-
     return (
         <MainSliderDiv>
-            <Swiper
-                spaceBetween={30}
-                slidesPerView={4}
-                autoplay={{
-                    delay: 2500,
-                    // disableOnInteraction: false,
-                    loopedSlides: 1,
-                    loop: true,
-                }}
-                pagination={{
-                    clickable: true,
-                }}
-                modules={[Autoplay, Pagination, Navigation]}
-                style={{
-                    width: "90%",
-                    height: "100%",
-                }}
-                navigation={{
-                    nextEl: ".swiper-next",
-                    prevEl: ".swiper-prev",
-                }}
-                breakpoints={{
-                    // 화면의 넓이가 320px 이상일 때 한개만 보여줌
-                    320: {
-                        slidesPerView: 1,
-                        spaceBetween: 30,
-                    },
-                    // 화면의 넓이가 640px 이상일 때
-                    640: {
-                        slidesPerView: 4,
-                        spaceBetween: 30,
-                    },
-                }}
-                onTransitionEnd={({ activeIndex }) => setActive(activeIndex)}
-            >
-                {items.map(({ id, title, urlimage }, idx) => (
-                    // console.log(title, urlimage)
-                    <SwiperSlide key={id}>
-                        <MainSliderItem
-                            title={title}
-                            urlimage={urlimage}
-                            active={idx === active}
-                        ></MainSliderItem>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+            <Temp>
+                <Swiper
+                    spaceBetween={30}
+                    slidesPerView={"auto"}
+                    autoplay={{
+                        delay: 2500,
+                        loopedSlides: 1,
+                        loop: true,
+                    }}
+                    centeredSlides={true}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    pagination={{ clickable: true }}
+                    style={{
+                        width: "90%",
+                        height: "100%",
+                        backgroundColor: "teal",
+                    }}
+                    navigation={true} // 네비게이션 적용, < >
+                    breakpoints={{
+                        // 화면의 넓이가 320px 이상일 때 한개만 보여줌
+                        350: {
+                            slidesPerView: "auto",
+                            spaceBetween: 30,
+                        },
+                    }}
+                    onTransitionEnd={({ activeIndex }) =>
+                        setActive(activeIndex)
+                    }
+                >
+                    {items.map(({ id, title, urlimage }, idx) => (
+                        <SwiperSlide
+                            key={id}
+                            style={{
+                                height: "450px",
+                                backgroundColor: "tomato",
+                            }}
+                            //className={idx === active ? "active" : ""}
+                            className="active"
+                        >
+                            <MainSliderItem
+                                title={title}
+                                urlimage={urlimage}
+                                active={idx === active}
+                            ></MainSliderItem>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </Temp>
         </MainSliderDiv>
     );
 }
--- src/components/Main/MainSlider/MainSliderItem.jsx
+++ src/components/Main/MainSlider/MainSliderItem.jsx
@@ -1,24 +1,32 @@
 import styled from "styled-components";
 
 const SliderItem = styled.div`
-    width: 172px;
-    height: 225px;
+    width: 200px;
+    height: 230px;
     border-radius: 20px;
-    /* display: flex;
-    justify-content: center;
-    align-items: center; */
 
     img {
         width: 100%;
         height: 100%;
         border-radius: 14px;
+        margin-top: 220px;
     }
 
     &.active {
         img {
             width: 200%;
-            height: 200%;
+            height: 450px;
             border-radius: 14px 0 0 14px;
+            margin-top: 0px;
+
+            @media all and (min-width: 480px) and (max-width: 767px) {
+                width: 100px;
+                height: 100px;
+            }
+            @media all and (max-width: 479px) {
+                width: 100px;
+                height: 100px;
+            }
         }
     }
 `;
