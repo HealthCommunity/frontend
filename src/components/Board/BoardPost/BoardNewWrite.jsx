cb613ee08705a4c7c9b5ec8b9d108cee1fe19b1d
pasong0511 <pasong0511@gmail.com>
2022-07-17 18:25:53 +0900

    Feat : 운동게시판 글쓰기 에디터 추가

--- src/components/Board/BoardPost/BoardNewWrite.jsx
+++ src/components/Board/BoardPost/BoardNewWrite.jsx
@@ -1,17 +1,80 @@
-import { InfoDiv, InfoTitle, BoardSummary } from "./BoardDetailStyle";
+import { InfoTitle } from "./BoardDetailStyle";
 import { useLocation } from "react-router";
 import Tiptap from "../../Editor/Tiptap";
+import Details from "../../Editor/Details";
+import "../../Editor/TiptapStyle.css";
+import { useState } from "react";
+import styled from "styled-components";
+
+const PostWrapper = styled.div`
+    max-width: 1040px;
+    padding: 60px 16px;
+    width: 1024px;
+    margin: 0 auto;
+    color: #333;
+    grid-gap: 50px;
+    gap: 50px px;
+    position: relative;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding-top: 150px;
+`;
+
+const PostTitleTitle = styled.input`
+    box-sizing: border-box;
+    width: 100%;
+    height: 56px;
+    min-height: 56px;
+    line-height: 44px;
+    box-shadow: none;
+    padding-left: 16px;
+    padding-right: 52px;
+    border: 1px solid grey;
+    border-radius: 5px;
+    font-size: 16px;
+    font-weight: 500;
+    color: #333;
+`;
+
+const TitleLabel = styled.label`
+    display: inline-block;
+    margin-bottom: 5px;
+    color: #333;
+    font-weight: 500;
+`;
 
 function BoardNewWrite() {
     const { pathname } = useLocation();
     const boardname = pathname.split("/")[1];
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const getContent = () => {
+        console.log(title, description);
+    };
+
+    const handleChange = (e) => {
+        setTitle(e.target.value);
+        console.log(title);
+    };
 
     return (
-        <InfoDiv>
+        <PostWrapper>
             <InfoTitle>{`${boardname} 글쓰기`}</InfoTitle>
-            <BoardSummary></BoardSummary>
-            <Tiptap />
-        </InfoDiv>
+            <TitleLabel Htmlfor="input-title">제목</TitleLabel>
+            <PostTitleTitle
+                id="input-title"
+                placeholder="글 제목을 입력해주세요!"
+                value={title}
+                onChange={handleChange}
+                autoComplete="off"
+            ></PostTitleTitle>
+            <Tiptap setDescription={setDescription} />
+            <label>미리보기</label>
+            <Details description={description} />
+            <button onClick={getContent}>내용 가져오기</button>
+        </PostWrapper>
     );
 }
 
--- src/components/Editor/Tiptap.jsx
+++ src/components/Editor/Tiptap.jsx
@@ -1,13 +1,133 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import Underline from "@tiptap/extension-underline";
+import {
+    FaBold,
+    FaHeading,
+    FaItalic,
+    FaListOl,
+    FaListUl,
+    FaQuoteLeft,
+    FaRedo,
+    FaStrikethrough,
+    FaUnderline,
+    FaUndo,
+} from "react-icons/fa";
 
-const Tiptap = () => {
+const MenuBar = ({ editor }) => {
+    if (!editor) {
+        return null;
+    }
+
+    return (
+        <div className="menuBar">
+            <div>
+                <button
+                    onClick={() => editor.chain().focus().toggleBold().run()}
+                    className={editor.isActive("bold") ? "is_active" : ""}
+                >
+                    <FaBold />
+                </button>
+                <button
+                    onClick={() => editor.chain().focus().toggleItalic().run()}
+                    className={editor.isActive("italic") ? "is_active" : ""}
+                >
+                    <FaItalic />
+                </button>
+                <button
+                    onClick={() =>
+                        editor.chain().focus().toggleUnderline().run()
+                    }
+                    className={editor.isActive("underline") ? "is_active" : ""}
+                >
+                    <FaUnderline />
+                </button>
+                <button
+                    onClick={() => editor.chain().focus().toggleStrike().run()}
+                    className={editor.isActive("strike") ? "is_active" : ""}
+                >
+                    <FaStrikethrough />
+                </button>
+                <button
+                    onClick={() =>
+                        editor.chain().focus().toggleHeading({ level: 2 }).run()
+                    }
+                    className={
+                        editor.isActive("heading", { level: 2 })
+                            ? "is_active"
+                            : ""
+                    }
+                >
+                    <FaHeading />
+                </button>
+                <button
+                    onClick={() =>
+                        editor.chain().focus().toggleHeading({ level: 3 }).run()
+                    }
+                    className={
+                        editor.isActive("heading", { level: 3 })
+                            ? "is_active"
+                            : ""
+                    }
+                >
+                    <FaHeading className="heading3" />
+                </button>
+                <button
+                    onClick={() =>
+                        editor.chain().focus().toggleBulletList().run()
+                    }
+                    className={editor.isActive("bulletList") ? "is_active" : ""}
+                >
+                    <FaListUl />
+                </button>
+                <button
+                    onClick={() =>
+                        editor.chain().focus().toggleOrderedList().run()
+                    }
+                    className={
+                        editor.isActive("orderedList") ? "is_active" : ""
+                    }
+                >
+                    <FaListOl />
+                </button>
+                <button
+                    onClick={() =>
+                        editor.chain().focus().toggleBlockquote().run()
+                    }
+                    className={editor.isActive("blockquote") ? "is_active" : ""}
+                >
+                    <FaQuoteLeft />
+                </button>
+            </div>
+            <div>
+                <button onClick={() => editor.chain().focus().undo().run()}>
+                    <FaUndo />
+                </button>
+                <button onClick={() => editor.chain().focus().redo().run()}>
+                    <FaRedo />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+const Tiptap = ({ setDescription }) => {
     const editor = useEditor({
-        extensions: [StarterKit],
-        content: "<p>Hello World!</p>",
+        extensions: [StarterKit, Underline],
+        content: ``,
+
+        onUpdate: ({ editor }) => {
+            const html = editor.getHTML();
+            setDescription(html);
+        },
     });
 
-    return <EditorContent editor={editor} />;
+    return (
+        <div className="textEditor">
+            <MenuBar editor={editor} />
+            <EditorContent editor={editor} />
+        </div>
+    );
 };
 
 export default Tiptap;
