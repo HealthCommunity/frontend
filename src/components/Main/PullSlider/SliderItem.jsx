269c62959c77f224cc53c2b6bb9bfb0220be20d8
pasong0511 <pasong0511@gmail.com>
2022-07-24 17:21:28 +0900

    Feat : 무한 슬라이드 구현, 페이지 네이션 버튼 클릭시 자연스럽게 이동하지 못하는 현상 확인

--- src/components/Main/PullSlider/SliderItem.jsx
+++ src/components/Main/PullSlider/SliderItem.jsx
@@ -1,21 +1,17 @@
-import { useEffect, useRef } from "react";
 import styled from "styled-components";
 
-const SliderItemInner = styled.li``;
+const SliderItemInner = styled.li`
+    position: absolute;
+`;
 
 const SliderItemImg = styled.img`
     width: 100vw;
     height: 480px;
 `;
 
-export default function SliderItem({ width, item }) {
-    // const ref = useRef();
-    // useEffect(() => {
-    //     console.log({ a: ref.current.clientWidth });
-    // });
-
+export default function SliderItem({ item, style }) {
     return (
-        <SliderItemInner style={{ width }}>
+        <SliderItemInner style={style}>
             <a href="#/">
                 <SliderItemImg src={item} alt=""></SliderItemImg>
             </a>
--- src/components/Main/PullSlider/SliderMain.jsx
+++ src/components/Main/PullSlider/SliderMain.jsx
@@ -2,7 +2,7 @@ import styled, { css } from "styled-components";
 import SliderItem from "./SliderItem";
 import SliderButton from "./SliderButton";
 
-import React, { useLayoutEffect, useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const SliderArea = styled.div`
     position: relative;
@@ -18,48 +18,60 @@ const Slider = styled.div`
 
 const SliderLists = styled.ul`
     position: relative;
+    display: flex;
     list-style: none;
     padding: 0px;
     background-color: tomato;
+    height: 480px;
 `;
 
-const SliderTrack = styled.div`
-    position: relative;
+const SliderPagenation = styled.ul`
+    position: absolute;
+    top: auto;
+    right: auto;
+    bottom: 8px;
+    left: 0;
+    display: block;
+    width: 100%;
+    white-space: nowrap;
+    text-align: center;
+`;
+
+const SliderPagenationList = styled.li`
     display: flex;
+    justify-content: center;
+    vertical-align: top;
+`;
+
+const SliderPageButton = styled.button`
+    border: 0;
+    display: block;
+    width: 8px;
+    height: 8px;
+    color: transparent;
+    border-radius: 4px;
+    background: #fff;
+    font-size: 0;
+    opacity: 0.8;
+    -webkit-transition: all 0.3s ease;
+    -o-transition: all 0.3s ease;
+    transition: all 0.3s ease;
+    margin: 0 0 0 12px;
+
+    ${(props) =>
+        props.isActive &&
+        css`
+            width: 36px;
+            opacity: 1;
+        `};
 `;
 
 export default function SliderMain({ data }) {
     const [width, setWidth] = useState(0);
-    const [currentIndex, setCurrentIndex] = useState(1); //현재 슬라이드의 index를 저장
-
-    const setSlides = () => {
-        let addedFront = [];
-        let addedLast = [];
-
-        addedLast.push(data[0]); //맨 앞에꺼 맨 뒤에 추가
-        addedFront.unshift(data[data.length - 1]); //맨 뒤에꺼 맨 앞으로 추가
-
-        return [...addedFront, ...data, ...addedLast];
-    };
-
-    let slides = setSlides();
+    const [currentIndex, setCurrentIndex] = useState(0); //현재 슬라이드의 index를 저장
 
     const handleSwipe = (direction) => {
-        //next 오른쪽 > 이동
-        if (direction === 1) {
-            setCurrentIndex(direction + currentIndex);
-            if (currentIndex > data.length) {
-                setCurrentIndex(1);
-                slideRef.current.style.transform = "translateX(0)";
-            }
-        }
-        //prev 왼쪽 < 이동
-        else {
-            setCurrentIndex(direction + currentIndex);
-            if (currentIndex <= 0) {
-                setCurrentIndex(data.length);
-            }
-        }
+        setCurrentIndex((prev) => prev + direction);
     };
 
     useEffect(() => {
@@ -73,12 +85,41 @@ export default function SliderMain({ data }) {
         };
     }, []);
 
-    const ref = useRef();
-    const slideRef = useRef();
+    //idx가 0보다 작은 경우
+    // - 0이거나, data.length
+    // idx임
+    const isOver = (idx) =>
+        idx < 0 ? 0 : idx >= data.length ? data.length - 1 : idx;
 
-    useEffect(() => {
-        console.log({ slideRef });
-    });
+    // const isOver = (idx) => {
+    //     if(idx < 0) {
+    //         return 0
+    //     }
+    //     return idx >= data.length ? data.length - 1 : idx;
+    // }
+
+    const ref = useRef();
+    const idx = Math.floor(currentIndex % data.length);
+    const startSlides = [3, 2, 1] //보여줘야할 currrentIndex보다 얼마나 멀리 떨어져 있는 지 나타냄
+        .map((num) => {
+            const nextIdx = idx >= 0 ? idx - num : data.length + idx - num;
+            //console.log(nextIdx);
+            if (nextIdx < 0) {
+                return data.length - Math.abs(nextIdx);
+            }
+            return nextIdx;
+        })
+        .map((num) => data[isOver(num)]);
+
+    const endSlides = [1, 2, 3]
+        .map((num) => {
+            const nextIdx = idx >= 0 ? idx + num : data.length + idx + num;
+            if (nextIdx < data.length) {
+                return nextIdx < 0 ? 0 : nextIdx;
+            }
+            return Math.abs(data.length - nextIdx);
+        })
+        .map((num) => data[isOver(num)]);
 
     return (
         <SliderArea ref={ref}>
@@ -91,25 +132,64 @@ export default function SliderMain({ data }) {
                     direction={false}
                     onClick={() => handleSwipe(1)}
                 />
-                <SliderLists className="slider-list">
-                    <SliderTrack
-                        ref={slideRef}
+                <SliderLists
+                    className="slider-list"
+                    style={{
+                        transform: `translate3d(${
+                            -currentIndex * width
+                        }px, 0px, 0px)`,
+                        transition: "all ease 0.4s",
+                    }}
+                >
+                    {startSlides.map((item, idx) => (
+                        <SliderItem
+                            key={currentIndex - 3 + idx}
+                            item={item}
+                            style={{
+                                width,
+                                left: (currentIndex - 3 + idx) * width,
+                            }}
+                        ></SliderItem>
+                    ))}
+                    <SliderItem
+                        key={currentIndex}
+                        item={data[idx >= 0 ? idx : data.length + idx]}
                         style={{
-                            transition: `all ease 0.4s`,
-                            transform: `translate3d(${
-                                -currentIndex * width
-                            }px, 0px, 0px)`,
+                            width,
+                            left: currentIndex * width,
                         }}
-                    >
-                        {slides.map((item, idx) => (
-                            <SliderItem
-                                key={idx}
-                                item={item}
-                                width={width}
-                            ></SliderItem>
-                        ))}
-                    </SliderTrack>
+                    ></SliderItem>
+                    {endSlides.map((item, idx) => (
+                        <SliderItem
+                            key={currentIndex + idx + 1}
+                            item={item}
+                            style={{
+                                width,
+                                left: (currentIndex + idx + 1) * width,
+                            }}
+                        ></SliderItem>
+                    ))}
                 </SliderLists>
+
+                <SliderPagenation>
+                    <SliderPagenationList>
+                        {data.map((item, num) => {
+                            const target = idx >= 0 ? idx : data.length + idx; //음수일 때는, 데이터 랭스에서 더함-> 음순니까 빼기된다.
+                            return (
+                                <SliderPageButton
+                                    key={num}
+                                    onClick={() => {
+                                        setCurrentIndex(
+                                            currentIndex - (target - num)
+                                        );
+                                    }}
+                                    className={num === target ? "active" : ""}
+                                    isActive={num === target}
+                                ></SliderPageButton>
+                            );
+                        })}
+                    </SliderPagenationList>
+                </SliderPagenation>
             </Slider>
         </SliderArea>
     );
