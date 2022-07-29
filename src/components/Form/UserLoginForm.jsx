d77a5aaff51838be3c7707acf0033148dc56dfd8
Ji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-07-29 18:20:08 +0900

    Feat : Login 설정변경

--- src/components/Form/UserLoginForm.jsx
+++ src/components/Form/UserLoginForm.jsx
@@ -8,32 +8,54 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export default function UserInputForm({ onSubmit }) {
-    const [usrInputs, setUsrInputs] = useState({
-        id: "",
-        password: "",
+  const [usrInputs, setUsrInputs] = useState({
+    id: "",
+    password: "",
+  });
+
+  const { id, password } = usrInputs;
+
+  const handleChange = (e) => {
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    setUsrInputs({
+      ...usrInputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value, // name 키를 가진 값을 value 로 설정
     });
+  };
 
-    const { id, password } = usrInputs;
+  const handleSubmit = (e) => {
+    e.preventDefault(); //새로고침 방지
 
-    const handleChange = (e) => {
-        const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
-        setUsrInputs({
-            ...usrInputs, // 기존의 input 객체를 복사한 뒤
-            [name]: value, // name 키를 가진 값을 value 로 설정
-        });
+    // return onSubmit(e, {
+    //     id: usrInputs.id,
+    //     password: usrInputs.password,
+    // });
+    console.log("로그인 시작", id, password);
+    console.log("로그인 재실행", id, password);
+
+    /* 데이터 전송 */
+    const myData = {
+      username: id,
+      password: password,
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault(); //새로고침 방지
-
-        // return onSubmit(e, {
-        //     id: usrInputs.id,
-        //     password: usrInputs.password,
-        // });
-        console.log("로그인 시작", id, password);
-        console.log("로그인 재실행", id, password);
-
-        /* 데이터 전송 */
+    fetch("http://54.166.132.169:8080/login", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(myData),
+    })
+      .then(function (response) {
+        console.log("성공", response);
+        alert("로그인에 성공하였습니다.");
+        //window.location.href = response.url;
+        //response.redirect(response.url);
+      })
+      .catch(function (error) {
+        // 오류발생시 실행
+        console.log("실패", error);
+        //window.location.href = "http://54.166.132.169:3200";
+      });
+    /*
         axios
             .post("http://54.166.132.169:8080/login", {
                 username: id,
@@ -50,58 +72,58 @@ export default function UserInputForm({ onSubmit }) {
                 console.log("실패", error);
                 //window.location.href = "http://54.166.132.169:3200";
             });
+            */
+    /*데이터 전송*/
+    // fetch("http://54.166.132.169:8080/login", {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //         username: id,
+    //         password: password,
+    //     }),
+    // })
+    //     .then((response) => response.json())
+    //     .then((data) => console.log(data));
 
-        /*데이터 전송*/
-        // fetch("http://54.166.132.169:8080/login", {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //     },
-        //     body: JSON.stringify({
-        //         username: id,
-        //         password: password,
-        //     }),
-        // })
-        //     .then((response) => response.json())
-        //     .then((data) => console.log(data));
+    /* 데이터 전송 URLSearchParams*/
+    // axios
+    //     .post(
+    //         "http://54.166.132.169:8080/login",
+    //         new URLSearchParams({
+    //             username: id,
+    //             password: password,
+    //         })
+    //     )
+    //     .then(function (response) {
+    //         console.log("성공", response);
+    //         alert("로그인에 성공하였습니다.");
+    //     })
+    //     .catch(function (error) {
+    //         // 오류발생시 실행
+    //         console.log("실패", error);
+    //     });
+  };
 
-        /* 데이터 전송 URLSearchParams*/
-        // axios
-        //     .post(
-        //         "http://54.166.132.169:8080/login",
-        //         new URLSearchParams({
-        //             username: id,
-        //             password: password,
-        //         })
-        //     )
-        //     .then(function (response) {
-        //         console.log("성공", response);
-        //         alert("로그인에 성공하였습니다.");
-        //     })
-        //     .catch(function (error) {
-        //         // 오류발생시 실행
-        //         console.log("실패", error);
-        //     });
-    };
+  return (
+    <UserFormGroup onSubmit={handleSubmit} style={{ height: "300px" }}>
+      <DescTextInput
+        type="text"
+        name="id"
+        placeholder="아이디"
+        onChange={handleChange}
+        required
+      />
 
-    return (
-        <UserFormGroup onSubmit={handleSubmit} style={{ height: "300px" }}>
-            <DescTextInput
-                type="text"
-                name="id"
-                placeholder="아이디"
-                onChange={handleChange}
-                required
-            />
-
-            <DescTextInput
-                type="password"
-                name="password"
-                placeholder="비밀번호"
-                onChange={handleChange}
-                required
-            />
-            <ButtonPupple type="submit" info={"로그인"}></ButtonPupple>
-        </UserFormGroup>
-    );
+      <DescTextInput
+        type="password"
+        name="password"
+        placeholder="비밀번호"
+        onChange={handleChange}
+        required
+      />
+      <ButtonPupple type="submit" info={"로그인"}></ButtonPupple>
+    </UserFormGroup>
+  );
 }
--- src/components/Form/UserSignForm.jsx
+++ src/components/Form/UserSignForm.jsx
@@ -7,141 +7,160 @@ import { ButtonPupple } from "../Share/ButtonPupple";
 import axios from "axios";
 
 const Label = styled.label`
-    margin-bottom: 10px;
-    font-size: ${(props) => props.theme.fontSizeH5};
+  margin-bottom: 10px;
+  font-size: ${(props) => props.theme.fontSizeH5};
 
-    @media all and (min-width: 480px) and (max-width: 767px) {
-        font-size: ${(props) => props.theme.fontSizeH6};
-        white-space: nowrap;
-        letter-spacing: -1px;
-    }
-    @media all and (max-width: 479px) {
-        font-size: ${(props) => props.theme.fontSizeH6};
-        white-space: nowrap;
-        letter-spacing: -1px;
-    }
+  @media all and (min-width: 480px) and (max-width: 767px) {
+    font-size: ${(props) => props.theme.fontSizeH6};
+    white-space: nowrap;
+    letter-spacing: -1px;
+  }
+  @media all and (max-width: 479px) {
+    font-size: ${(props) => props.theme.fontSizeH6};
+    white-space: nowrap;
+    letter-spacing: -1px;
+  }
 `;
 
 const CheckInput = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-    margin: 0px 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  margin: 0px 0px;
 
-    input {
-        margin: 0 8px 0 0;
-    }
-    label {
-        margin-top: 8px;
-    }
+  input {
+    margin: 0 8px 0 0;
+  }
+  label {
+    margin-top: 8px;
+  }
 `;
 
 function UserSignForm({ onSubmit }) {
-    const [usrInputs, setUsrInputs] = useState({
-        id: "",
-        password: "",
-        checkPassword: "",
-        nickname: "",
-        isAgree: false,
+  const [usrInputs, setUsrInputs] = useState({
+    id: "",
+    password: "",
+    checkPassword: "",
+    nickname: "",
+    isAgree: false,
+  });
+
+  const { id, password, checkPassword, nickname, isAgree } = usrInputs;
+
+  const handleChange = (e) => {
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    setUsrInputs({
+      ...usrInputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value, // name 키를 가진 값을 value 로 설정
     });
+  };
 
-    const { id, password, checkPassword, nickname, isAgree } = usrInputs;
+  const handleIsAgree = (e) => {
+    const { checked, name } = e.target;
 
-    const handleChange = (e) => {
-        const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
-        setUsrInputs({
-            ...usrInputs, // 기존의 input 객체를 복사한 뒤
-            [name]: value, // name 키를 가진 값을 value 로 설정
-        });
-    };
+    setUsrInputs({
+      ...usrInputs,
+      [name]: checked,
+    });
+  };
 
-    const handleIsAgree = (e) => {
-        const { checked, name } = e.target;
+  const checkValidityId = (id) => {
+    if (id.length < 5) {
+      return [false, "아이디 5~12자 문자입니다."];
+    } else if (id.length > 12) {
+      return [false, "아이디 5~12자 문자입니다."];
+    } else {
+      if (isRuleId(id)) {
+        return [true, "사용 가능 아이디입니다."];
+      }
+      return [false, "유효하지 않은 아이디입니다."];
+    }
+  };
 
-        setUsrInputs({
-            ...usrInputs,
-            [name]: checked,
-        });
-    };
+  const checkValidityPassword = (password) => {
+    if (password.length < 8) {
+      return [false, "비밀번호 8~16자, 숫자, 문자, 특수문자입니다."];
+    } else if (password.length > 16) {
+      return [false, "비밀번호 8~16자, 숫자, 문자, 특수문자입니다."];
+    } else {
+      if (isRulePassword(password)) {
+        return [true, "사용 가능 비밀번호입니다."];
+      }
+      return [false, "유효하지 않은 비밀번호입니다."];
+    }
+  };
 
-    const checkValidityId = (id) => {
-        if (id.length < 5) {
-            return [false, "아이디 5~12자 문자입니다."];
-        } else if (id.length > 12) {
-            return [false, "아이디 5~12자 문자입니다."];
-        } else {
-            if (isRuleId(id)) {
-                return [true, "사용 가능 아이디입니다."];
-            }
-            return [false, "유효하지 않은 아이디입니다."];
-        }
-    };
+  const checkDoublePassword = (password) => {
+    if (password !== usrInputs.password) {
+      return [false, "비밀번호가 일치하지 않습니다."];
+    } else {
+      return [true, "동일한 비밀번호입니다."];
+    }
+  };
 
-    const checkValidityPassword = (password) => {
-        if (password.length < 8) {
-            return [false, "비밀번호 8~16자, 숫자, 문자, 특수문자입니다."];
-        } else if (password.length > 16) {
-            return [false, "비밀번호 8~16자, 숫자, 문자, 특수문자입니다."];
-        } else {
-            if (isRulePassword(password)) {
-                return [true, "사용 가능 비밀번호입니다."];
-            }
-            return [false, "유효하지 않은 비밀번호입니다."];
-        }
-    };
+  const checkValidityNickname = (nickname) => {
+    if (nickname.length < 2) {
+      return [false, "닉네임은 2~10자 문자입니다."];
+    } else if (nickname.length > 10) {
+      return [false, "닉네임은 2~10자 문자입니다."];
+    } else {
+      if (isRuleNick(nickname)) {
+        return [true, "올바른 닉네임입니다."];
+      }
+      return [false, "유효하지 않는 닉네임입니다."];
+    }
+  };
 
-    const checkDoublePassword = (password) => {
-        if (password !== usrInputs.password) {
-            return [false, "비밀번호가 일치하지 않습니다."];
-        } else {
-            return [true, "동일한 비밀번호입니다."];
-        }
-    };
+  const isRuleId = (asValue) => {
+    const regExp = /^[a-z]+[a-z0-9]{5,19}$/g;
+    return regExp.test(asValue);
+  };
 
-    const checkValidityNickname = (nickname) => {
-        if (nickname.length < 2) {
-            return [false, "닉네임은 2~10자 문자입니다."];
-        } else if (nickname.length > 10) {
-            return [false, "닉네임은 2~10자 문자입니다."];
-        } else {
-            if (isRuleNick(nickname)) {
-                return [true, "올바른 닉네임입니다."];
-            }
-            return [false, "유효하지 않는 닉네임입니다."];
-        }
-    };
+  const isRulePassword = (asValue) => {
+    const regExp =
+      /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
+    return regExp.test(asValue);
+  };
 
-    const isRuleId = (asValue) => {
-        const regExp = /^[a-z]+[a-z0-9]{5,19}$/g;
-        return regExp.test(asValue);
-    };
+  const isRuleNick = (asValue) => {
+    const regExp = /^[a-zA-Zㄱ-힣][a-zA-Zㄱ-힣 ]*$/;
+    return regExp.test(asValue);
+  };
 
-    const isRulePassword = (asValue) => {
-        const regExp =
-            /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/;
-        return regExp.test(asValue);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const isRuleNick = (asValue) => {
-        const regExp = /^[a-zA-Zㄱ-힣][a-zA-Zㄱ-힣 ]*$/;
-        return regExp.test(asValue);
-    };
+    if (!isAgree) {
+      return alert("동의 해주세요.");
+    }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    if (
+      checkValidityId(id)[0] &&
+      checkValidityPassword(password)[0] &&
+      checkDoublePassword(password, checkPassword)[0] &&
+      checkValidityNickname(nickname)[0]
+    ) {
+      const myData = {
+        loginId: id,
+        password: password,
+        passwordCheck: checkPassword,
+        nickName: nickname,
+      };
 
-        if (!isAgree) {
-            return alert("동의 해주세요.");
-        }
-
-        if (
-            checkValidityId(id)[0] &&
-            checkValidityPassword(password)[0] &&
-            checkDoublePassword(password, checkPassword)[0] &&
-            checkValidityNickname(nickname)[0]
-        ) {
-            /* 데이터 전송 */
+      fetch("http://54.166.132.169:8080/api/user/join", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(myData),
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          console.log(response);
+          alert("서브밋 완료");
+        })
+        .catch((error) => console.error(error.message));
+      /* 데이터 전송 */
+      /*
             axios
                 .post("http://54.166.132.169:8080/api/user/join", {
                     loginId: id,
@@ -157,80 +176,80 @@ function UserSignForm({ onSubmit }) {
                     // 오류발생시 실행
                     console.log("실패", error);
                 });
+                */
+      /* 데이터 전송 URLSearchParams*/
+      // axios
+      //     .post(
+      //         "http://54.166.132.169:8080/api/user/join",
+      //         new URLSearchParams({
+      //             loginId: id,
+      //             password: password,
+      //             passwordCheck: checkPassword,
+      //             nickName: nickname,
+      //         })
+      //     )
+      //     .then(function (response) {
+      //         console.log("성공", response);
+      //         alert("회원가입에 성공하였습니다.");
+      //     })
+      //     .catch(function (error) {
+      //         // 오류발생시 실행
+      //         console.log("실패", error);
+      //     });
+    }
+  };
 
-            /* 데이터 전송 URLSearchParams*/
-            // axios
-            //     .post(
-            //         "http://54.166.132.169:8080/api/user/join",
-            //         new URLSearchParams({
-            //             loginId: id,
-            //             password: password,
-            //             passwordCheck: checkPassword,
-            //             nickName: nickname,
-            //         })
-            //     )
-            //     .then(function (response) {
-            //         console.log("성공", response);
-            //         alert("회원가입에 성공하였습니다.");
-            //     })
-            //     .catch(function (error) {
-            //         // 오류발생시 실행
-            //         console.log("실패", error);
-            //     });
-        }
-    };
+  return (
+    <UserFormGroup onSubmit={handleSubmit} style={{ height: "450px" }}>
+      <DescTextInput
+        type="text"
+        name="id"
+        placeholder="아이디"
+        onChange={handleChange}
+        onValidation={checkValidityId}
+        required
+      />
 
-    return (
-        <UserFormGroup onSubmit={handleSubmit} style={{ height: "450px" }}>
-            <DescTextInput
-                type="text"
-                name="id"
-                placeholder="아이디"
-                onChange={handleChange}
-                onValidation={checkValidityId}
-                required
-            />
+      <DescTextInput
+        type="password"
+        name="password"
+        placeholder="비밀번호"
+        onChange={handleChange}
+        onValidation={checkValidityPassword}
+        required
+      />
 
-            <DescTextInput
-                type="password"
-                name="password"
-                placeholder="비밀번호"
-                onChange={handleChange}
-                onValidation={checkValidityPassword}
-                required
-            />
-
-            <DescTextInput
-                type="password"
-                name="checkPassword"
-                placeholder="비밀번호 확인"
-                onChange={handleChange}
-                onValidation={checkDoublePassword}
-                required
-            />
-            <DescTextInput
-                type="text"
-                name="nickname"
-                placeholder="닉네임"
-                onChange={handleChange}
-                onValidation={checkValidityNickname}
-                required
-            />
-            <CheckInput>
-                <input
-                    type="checkbox"
-                    id="agree"
-                    checked={isAgree}
-                    name="isAgree"
-                    onChange={handleIsAgree}
-                />
-                <Label htmlFor="agree">
-                    이용약관 및 개인정보 수집∙이용에 동의합니다.
-                </Label>
-            </CheckInput>
-            <ButtonPupple type="submit" info={"가입하기"}></ButtonPupple>
-        </UserFormGroup>
-    );
+      <DescTextInput
+        type="password"
+        name="checkPassword"
+        placeholder="비밀번호 확인"
+        onChange={handleChange}
+        onValidation={checkDoublePassword}
+        required
+      />
+      <DescTextInput
+        type="text"
+        name="nickname"
+        placeholder="닉네임"
+        onChange={handleChange}
+        onValidation={checkValidityNickname}
+        required
+      />
+      <CheckInput>
+        <input
+          type="checkbox"
+          id="agree"
+          checked={isAgree}
+          name="isAgree"
+          onChange={handleIsAgree}
+        />
+        <Label htmlFor="agree">
+          이용약관 및 개인정보 수집∙이용에 동의합니다.
+        </Label>
+      </CheckInput>
+      <ButtonPupple type="submit" info={"가입하기"}></ButtonPupple>
+    </UserFormGroup>
+  );
 }
 
 export default UserSignForm;
