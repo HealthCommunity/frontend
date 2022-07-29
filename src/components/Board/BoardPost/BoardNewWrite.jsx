3312af9e4ea13de43ccc8411222d06fac61effb3
Ji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-07-29 18:58:17 +0900

    Feat : 게시판 글쓰기 input 숫자 plus, minus

--- src/components/Board/BoardPost/BoardNewWrite.jsx
+++ src/components/Board/BoardPost/BoardNewWrite.jsx
@@ -7,105 +7,108 @@ import { useRef, useState } from "react";
 import styled from "styled-components";
 
 const PostWrapper = styled.div`
-    max-width: 1040px;
-    padding: 60px 16px;
-    width: 1024px;
-    margin: 0 auto;
-    color: #333;
-    grid-gap: 50px;
-    gap: 50px px;
-    position: relative;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    padding-top: 150px;
+  max-width: 1040px;
+  padding: 60px 16px;
+  width: 1024px;
+  margin: 0 auto;
+  color: #333;
+  grid-gap: 50px;
+  gap: 50px px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 150px;
 `;
 
 const PostTitleTitle = styled.input`
-    box-sizing: border-box;
-    width: 100%;
-    height: 56px;
-    min-height: 56px;
-    line-height: 44px;
-    box-shadow: none;
-    padding-left: 16px;
-    padding-right: 52px;
-    border: 1px solid grey;
-    border-radius: 5px;
-    font-size: 16px;
-    font-weight: 500;
-    color: #333;
+  box-sizing: border-box;
+  width: 100%;
+  height: 56px;
+  min-height: 56px;
+  line-height: 44px;
+  box-shadow: none;
+  padding-left: 16px;
+  padding-right: 52px;
+  border: 1px solid grey;
+  border-radius: 5px;
+  font-size: 16px;
+  font-weight: 500;
+  color: #333;
 `;
 
 const TitleLabel = styled.label`
-    display: inline-block;
-    margin-bottom: 5px;
-    color: #333;
-    font-weight: 500;
+  display: inline-block;
+  margin-bottom: 5px;
+  color: #333;
+  font-weight: 500;
 `;
 
-
-
 function BoardNewWrite() {
-    const { pathname } = useLocation();
-    const boardname = pathname.split("/")[1];
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
+  const { pathname } = useLocation();
+  const boardname = pathname.split("/")[1];
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-    const getContent = () => {
-        console.log(title, description);
+  const getContent = () => {
+    console.log(title, description);
+  };
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+    console.log(title);
+  };
+  const inputlist = useRef(1);
+  const [inputItem, setInputItem] = useState([{ id: 0, title: "" }]);
+  function addInput() {
+    const input = {
+      id: inputlist.current,
+      title: "",
     };
+    setInputItem([...inputItem, input]); //아이디값에 번호 하나씩 추가
+    inputlist.current += 1;
+  }
+  console.log(inputItem);
+  function deleteInput() {
+    const deleteInput = inputItem.slice(0, -1);
+    setInputItem(deleteInput);
+  }
+  function handleChangeInput(e, index) {
+    if (index > inputItem.length) return;
+    const inputItemsCopy = JSON.parse(JSON.stringify(inputItem));
+    inputItemsCopy[index].title = e.target.value;
+    setInputItem(inputItemsCopy);
+  }
 
-    const handleChange = (e) => {
-        setTitle(e.target.value);
-        console.log(title);
-    }
-    const inputlist = useRef(1);
-    const [ inputItem, setInputItem] = useState([{id: 0, title: ""}]);
-    function addInput (){
-        const input = {
-            id: inputlist.current,
-            title: "",
-        };
-        setInputItem([...inputItem, input]);  //아이디값에 번호 하나씩 추가
-        inputlist.current+=1;
-    }
-
-    function handleChangeInput(e, index){
-        if(index > inputItem.length) return;
-        const inputItemsCopy= JSON.parse(JSON.stringify(inputItem));
-        inputItemsCopy[index].title = e.target.value;
-        setInputItem(inputItemsCopy);
-    }
-    
-    return (
-        <PostWrapper>
-            <InfoTitle>{`${boardname} 글쓰기`}</InfoTitle>
-            <TitleLabel Htmlfor="input-title">제목</TitleLabel>
-            <PostTitleTitle
-                id="input-title"
-                placeholder="글 제목을 입력해주세요!"
-                value={title}
-                onChange={handleChange}
-                autoComplete="off"
-            ></PostTitleTitle>
-            <button onClick={addInput}> + </button>
-            {inputItem?.map((item, index) => (
-                <div key={index}>
-                <input
-                    type="file"
-                    className={`title-${index}`}
-                    onChange={e => handleChangeInput(e, index)}
-                    value={item.title || ""}
-                />
-                </div>
-            ))}
-            <Tiptap setDescription={setDescription} />
-            <label>미리보기</label>
-            <Details description={description} />
-            <button onClick={getContent}>내용 가져오기</button>
-        </PostWrapper>
-    );
+  return (
+    <PostWrapper>
+      <InfoTitle>{`${boardname} 글쓰기`}</InfoTitle>
+      <TitleLabel Htmlfor="input-title">제목</TitleLabel>
+      <PostTitleTitle
+        id="input-title"
+        placeholder="글 제목을 입력해주세요!"
+        value={title}
+        onChange={handleChange}
+        autoComplete="off"
+      ></PostTitleTitle>
+      <button onClick={addInput}> + </button>
+      <button onClick={deleteInput}> - </button>
+      {inputItem?.map((item, index) => (
+        <div key={index}>
+          <input
+            type="file"
+            className={`title-${index}`}
+            onChange={(e) => handleChangeInput(e, index)}
+            value={item.title || ""}
+          />
+        </div>
+      ))}
+      <Tiptap setDescription={setDescription} />
+      <label>미리보기</label>
+      <Details description={description} />
+      <button onClick={getContent}>내용 가져오기</button>
+    </PostWrapper>
+  );
 }
 
 export default BoardNewWrite;
