36cd2d30c65ef959ddd03a61dee48855d881c140
Ji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-08-18 23:41:49 +0900

    fix : user 데이터 패칭 오류 수정

--- src/pages/Exercise.jsx
+++ src/pages/Exercise.jsx
@@ -10,47 +10,45 @@ import AdverSlider from "../components/Slider/AdverSlider/AdverSlider";
 import slide1 from "../assets/images/main-slide-img01.png";
 import slide2 from "../assets/images/main-slide-img02.png";
 import slide3 from "../assets/images/main-slide-img03.png";
+import useUserData from "../api/useUserData";
 
 const ModalButton = styled.button`
-    height: 45px;
-    background: #fcfcfd;
-    border: 1px solid #e8eaee;
-    box-sizing: border-box;
-    border-radius: 10px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-    cursor: pointer;
-    min-width: 200px;
-    padding: 0 10px;
-    transition: all 0.4s;
+  height: 45px;
+  background: #fcfcfd;
+  border: 1px solid #e8eaee;
+  box-sizing: border-box;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  cursor: pointer;
+  min-width: 200px;
+  padding: 0 10px;
+  transition: all 0.4s;
 `;
 
 export default function Exercise() {
-    const data = [slide1, slide2, slide3];
-
-    const useLogin = useRecoilValue(isLogin);
-    return (
-        <>
-            <Title name="exercise" />
-            <Nav />
-            <WidthAreaSpace>
-                <AdverSlider data={data} />
-                {useLogin ? (
-                    <ModalButton>
-                        <Link to={"write"}>글쓰기</Link>
-                    </ModalButton>
-                ) : null}
-
-                <GroupItem category="exercisepost">
-                    <BoardFetchItems category="exercisepost" />
-                </GroupItem>
-            </WidthAreaSpace>
-        </>
-    );
+  const data = [slide1, slide2, slide3];
+  const [userData] = useUserData();
+  return (
+    <>
+      <Title name="exercise" />
+      <Nav />
+      <WidthAreaSpace>
+        {userData ? (
+          <ModalButton>
+            <Link to={"write"}>글쓰기</Link>
+          </ModalButton>
+        ) : null}
+        <GroupItem category="threepowerpost">
+          <BoardFetchItems category="threepowerpost" />
+        </GroupItem>
+      </WidthAreaSpace>
+    </>
+  );
 }
 
 function GroupItem({ children }) {
-    return <>{children}</>;
+  return <>{children}</>;
 }
