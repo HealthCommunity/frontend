a36f537894818c66f2f4b9ed7b1f92cfe64b1d2a
pasong0511 <pasong0511@gmail.com>
2022-08-21 23:22:34 +0900

    Feat : 마이데이터 삼대력 차트 제작 완료

--- src/components/Profile/ContentBody.jsx
+++ src/components/Profile/ContentBody.jsx
@@ -9,78 +9,72 @@ import useUserData from "../../api/useUserData";
 import ProfileChart from "./ProfileChart";
 
 function Group({ children, selected }) {
-    const elements = React.Children.toArray(children);
-    return <>{elements.find(({ props }) => selected === props.name)}</>;
+  const elements = React.Children.toArray(children);
+  return <>{elements.find(({ props }) => selected === props.name)}</>;
 }
 
 function GroupItem({ children }) {
-    return <>{children}</>;
+  return <>{children}</>;
 }
 
 export default function ContentBody() {
-    const [selected, setSelected] = useState("profile");
-    const [userData, reFetch] = useUserData(); //로그인 상태 유저 데이터 가져옴
+  const [selected, setSelected] = useState("profile");
+  const [userData, reFetch] = useUserData(); //로그인 상태 유저 데이터 가져옴
 
-    useEffect(() => {
-        reFetch();
-    }, []);
+  useEffect(() => {
+    reFetch();
+  }, []);
 
-    if (!userData) {
-        return <></>;
-    }
+  if (!userData) {
+    return <></>;
+  }
 
-    const { nickName, loginId, bigThreePower } = userData;
-    const myPowerData = [
-        bigThreePower.bench,
-        bigThreePower.dead,
-        bigThreePower.squat,
-    ];
-    console.log(myPowerData);
+  const { nickName, loginId, bigThreePower } = userData;
+  const myPowerData = [
+    bigThreePower.bench,
+    bigThreePower.dead,
+    bigThreePower.squat,
+    bigThreePower.sum,
+  ];
 
-    return (
-        <ProfileContainer>
-            <ProfileNav
-                onChange={(name) => {
-                    setSelected(name);
-                }}
-                typeName={selected}
-            />
-            <Group selected={selected}>
-                <GroupItem name="profile">
-                    <ProfileContainerMyInfo>
-                        {!!userData && (
-                            <ProfileContent>
-                                <ProfileChart
-                                    PowerData={myPowerData}
-                                ></ProfileChart>
-                                <div>{loginId}</div>
-                                <div>{nickName}</div>
-                            </ProfileContent>
-                        )}
-                    </ProfileContainerMyInfo>
+  return (
+    <ProfileContainer>
+      <ProfileNav
+        onChange={(name) => {
+          setSelected(name);
+        }}
+        typeName={selected}
+      />
+      <Group selected={selected}>
+        <GroupItem name="profile">
+          <ProfileContainerMyInfo>
+            {!!userData && (
+              <ProfileContent>
+                <ProfileChart PowerData={myPowerData}></ProfileChart>
+                <div>{loginId}</div>
+                <div>{nickName}</div>
+              </ProfileContent>
+            )}
+          </ProfileContainerMyInfo>
 
-                    <>{!!userData && <BoardFetchItems category={"user"} />}</>
-                </GroupItem>
-                <GroupItem name="profileModify">
-                    {!!userData && (
-                        <ContentUserModify
-                            loginId={loginId}
-                        ></ContentUserModify>
-                    )}
-                </GroupItem>
-                <GroupItem name="userDropOut">
-                    {!!userData && (
-                        <ContentUserDropOut
-                            loginId={loginId}
-                        ></ContentUserDropOut>
-                    )}
-                </GroupItem>
-            </Group>
-        </ProfileContainer>
-    );
+          <>{!!userData && <BoardFetchItems category={"user"} />}</>
+        </GroupItem>
+        <GroupItem name="profileModify">
+          {!!userData && (
+            <ContentUserModify loginId={loginId}></ContentUserModify>
+          )}
+        </GroupItem>
+        <GroupItem name="userDropOut">
+          {!!userData && (
+            <ContentUserDropOut loginId={loginId}></ContentUserDropOut>
+          )}
+        </GroupItem>
+      </Group>
+    </ProfileContainer>
+  );
 }
 
 export const ProfileContent = styled.div`
-    display: flex;
-    margin: auto;
+  display: flex;
+  margin: auto;
 `;
--- src/components/Profile/ProfileChart.jsx
+++ src/components/Profile/ProfileChart.jsx
@@ -2,77 +2,153 @@ import styled from "styled-components";
 import ApexCharts from "react-apexcharts";
 
 function setChartOption() {
-    const ChartOptions = {
-        options: {
-            chart: {
-                type: "donut",
-            },
-            legend: {
-                position: "bottom",
-            },
-            responsive: [
-                {
-                    breakpoint: 480,
-                },
-            ],
-            plotOptions: {
-                pie: {
-                    donut: {
-                        size: "80%",
-                        labels: {
-                            show: true,
-                            total: {
-                                showAlways: true,
-                                show: true,
-                                label: "삼대력",
-                                fontFamily: "Pretendard",
-                                fontStyle: "normal",
-                                fontWeight: "400",
-                                fontSize: "18px",
-                                lineHeight: "26px",
-                                textAlign: "center",
-                                color: "#888888",
-                            },
-                            value: {
-                                show: true,
-                                fontFamily: "Pretendard",
-                                fontStyle: "normal",
-                                fontWeight: "700",
-                                fontSize: "36px",
-                                lineHeight: "44px",
-                                textAlign: "center",
-                                color: "#222222",
-                            },
-                        },
-                    },
-                },
-            },
-            labels: ["데드리프트", "벤치프레스", "스쿼트"],
-            fill: {
-                opacity: 1,
-                colors: ["#0066FF", "#7AFFBF", "#00D1FF"],
-            },
+  const ChartOptions = {
+    options: {
+      chart: {
+        type: "donut",
+      },
+      legend: {
+        show: false,
+      },
+      responsive: [
+        {
+          breakpoint: 480,
         },
-    };
+      ],
+      dataLabels: {
+        enabled: false,
+      },
+      plotOptions: {
+        pie: {
+          donut: {
+            size: "95%",
+          },
+        },
+      },
+      labels: ["데드리프트", "벤치프레스", "스쿼트"],
+      fill: {
+        opacity: 1,
+        colors: ["#0066FF", "#7AFFBF", "#00D1FF"],
+      },
+      tooltip: {
+        enabled: true,
+        y: {
+          formatter: function (val) {
+            return val + "kg";
+          },
+          title: {
+            formatter: function (seriesName) {
+              return "";
+            },
+          },
+        },
+      },
+      stroke: {
+        show: false, //차트 border
+      },
+    },
+  };
 
-    return ChartOptions.options;
+  return ChartOptions.options;
 }
 
 export default function ProfileChart({ PowerData }) {
-    return (
-        <HomeChartDiv>
-            <ApexCharts
-                options={setChartOption()}
-                series={PowerData}
-                type="donut"
-                width="500"
-            />
-        </HomeChartDiv>
-    );
+  return (
+    <ChartsBox>
+      <ApexCharts
+        options={setChartOption()}
+        series={PowerData.slice(0, 3)}
+        type="donut"
+        width="300"
+        height="300"
+      />
+      <>
+        <InnerTop>
+          <InfoSumTitle>3대력</InfoSumTitle>
+          <InfoSumValue>{PowerData[3]}</InfoSumValue>
+        </InnerTop>
+        <InnderBottom>
+          <div>
+            <InfoPowerTitle>벤치프레스</InfoPowerTitle>
+            <InfoPowerValue style={{ color: "#0066ff" }}>
+              {PowerData[0]}
+            </InfoPowerValue>
+          </div>
+          <div>
+            <InfoPowerTitle>데드리프트</InfoPowerTitle>
+            <InfoPowerValue style={{ color: "#7affbf" }}>
+              {PowerData[1]}
+            </InfoPowerValue>
+          </div>
+          <div>
+            <InfoPowerTitle>스쿼트</InfoPowerTitle>
+            <InfoPowerValue style={{ color: "#00d1ff" }}>
+              {PowerData[2]}
+            </InfoPowerValue>
+          </div>
+        </InnderBottom>
+      </>
+    </ChartsBox>
+  );
 }
 
-const HomeChartDiv = styled.div`
-    width: 500px;
-    height: 400px;
-    margin: 100px 20px;
+const ChartsBox = styled.div`
+  position: relative;
+  width: 300px;
+`;
+
+const InnerTop = styled.div`
+  position: absolute;
+  top: 66px;
+  left: 110px;
+`;
+
+const InfoSumTitle = styled.p`
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 26px;
+  text-align: center;
+  color: #888888;
+`;
+
+const InfoSumValue = styled.p`
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 44px;
+  text-align: center;
+  color: #222222;
+`;
+
+const InnderBottom = styled.div`
+  position: absolute;
+  top: 166px;
+  left: 60px;
+  width: 180px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 10;
+`;
+
+const InfoPowerTitle = styled.span`
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+  text-align: center;
+  color: #888888;
+`;
+
+const InfoPowerValue = styled.div`
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 26px;
+  text-align: center;
 `;
