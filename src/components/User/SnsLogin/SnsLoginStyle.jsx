6303148a0ae9f9e2a35fe253760fac10e02c1aa1
pasong0511 <pasong0511@gmail.com>
2022-08-25 22:44:10 +0900

    style : 로그인, 회원가입 레이아웃 수정

--- src/components/User/SnsLogin/SnsLoginStyle.jsx
+++ src/components/User/SnsLogin/SnsLoginStyle.jsx
@@ -1,48 +1,45 @@
 import styled from "styled-components";
 
 export const SelectTabBar = styled.div`
-    display: flex;
-    margin-top: 28px;
-    width: 100%;
-    background-color: tomato;
+  display: flex;
+  margin-top: 28px;
+  width: 100%;
+  margin-bottom: 28px;
 `;
 
 export const SelectTabItem = styled.div`
-    flex: 1 1;
-    font-size: ${(props) => props.theme.fontSizeH4};
-    font-weight: 400;
-    font-size: 18px;
-    line-height: 26px;
-    text-align: center;
-    cursor: pointer;
-    font-weight: ${(props) => (props.isActive ? 700 : 400)};
-    color: ${(props) =>
-        props.isActive
-            ? props.theme.colorPointPupple200
-            : props.theme.colorFontGrey100};
-    a {
-        display: block;
-    }
+  flex: 1 1;
+  font-size: ${(props) => props.theme.fontSizeH4};
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 26px;
+  text-align: center;
+  cursor: pointer;
+  font-weight: ${(props) => (props.isActive ? 700 : 400)};
+  color: ${(props) => (props.isActive ? "#0066FF" : "#888888")};
+  a {
+    display: block;
+  }
 `;
 
 export const SnsLinkList = styled.ul`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const SnsLinkItem = styled.li`
-    img {
-        width: 100%;
-        height: 70px;
-    }
+  img {
+    width: 100%;
+    height: 70px;
+  }
 `;
 
 export const RowGroup = styled.div`
-    display: flex;
-    width: 100%;
-    justify-content: space-evenly;
-    margin-top: 32px;
+  display: flex;
+  width: 100%;
+  justify-content: space-evenly;
+  margin-top: 32px;
 `;
--- src/pages/Sign.jsx
+++ src/pages/Sign.jsx
@@ -1,57 +1,51 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import Title from "../utils/Title/Title";
 import Nav from "../components/Navbar/index";
 import UserSign from "../components/User/UserSign";
-import { SnsSelectTab } from "../components/User/SnsLogin/SnsSelectTab";
+import {
+  SnsSelectTab,
+  SNSGroup,
+  GroupItem,
+} from "../components/User/SnsLogin/SnsSelectTab";
 import { WidthAreaSpace } from "../styles/Layout/CommonLayout";
 import { SnsLogin } from "../components/User/SnsLogin/SnsLogin";
 import {
-    ContentBackgroud,
-    ContentContainer,
-    LoginContainerArea,
+  ContentBackgroud,
+  ContentContainer,
+  LoginContainerArea,
 } from "../components/User/LoginLayout";
 
 function Sign() {
-    const [selected, setSelected] = useState("emaillogin");
-    return (
-        <>
-            <Title name="Sign" />
-            <Nav />
-            <WidthAreaSpace>
-                <ContentBackgroud>
-                    <ContentContainer>
-                        <LoginContainerArea>
-                            <SnsSelectTab
-                                onChange={(name) => {
-                                    setSelected(name);
-                                }}
-                                typeName={selected}
-                            />
-                            <Group selected={selected}>
-                                <GroupItem name="emaillogin">
-                                    <UserSign />
-                                </GroupItem>
-
-                                <GroupItem name="snslogin">
-                                    <SnsLogin />
-                                </GroupItem>
-                            </Group>
-                        </LoginContainerArea>
-                    </ContentContainer>
-                </ContentBackgroud>
-            </WidthAreaSpace>
-        </>
-    );
-}
-
-function Group({ children, selected }) {
-    const elements = React.Children.toArray(children);
-    return <>{elements.find(({ props }) => selected === props.name)}</>;
-}
-
-function GroupItem({ children }) {
-    return <>{children}</>;
+  const [selected, setSelected] = useState("emaillogin");
+  return (
+    <>
+      <Title name="Sign" />
+      <Nav />
+      <WidthAreaSpace>
+        <ContentBackgroud>
+          <ContentContainer>
+            <LoginContainerArea>
+              <SnsSelectTab
+                onChange={(name) => {
+                  setSelected(name);
+                }}
+                typeName={selected}
+                text={"가입"}
+              />
+              <SNSGroup selected={selected}>
+                <GroupItem name="emaillogin">
+                  <UserSign />
+                </GroupItem>
+                <GroupItem name="snslogin">
+                  <SnsLogin />
+                </GroupItem>
+              </SNSGroup>
+            </LoginContainerArea>
+          </ContentContainer>
+        </ContentBackgroud>
+      </WidthAreaSpace>
+    </>
+  );
 }
 
 export default Sign;
