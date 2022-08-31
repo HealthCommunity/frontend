69a75cb3526500132cef2b4f3e87ad63ec6addf5
pasong0511 <pasong0511@gmail.com>
2022-08-31 22:03:13 +0900

    feat : 게시판 상단에 추가할 배경 컴포넌트 추가

--- src/components/Slider/AdverSlider/AdverSlider.jsx
+++ src/components/Slider/AdverSlider/AdverSlider.jsx
@@ -2,58 +2,58 @@ import styled from "styled-components";
 import SliderMain from "../PullSlider/SliderMain";
 
 export default function AdverSlider({ data }) {
-    return (
-        <AdverBackground>
-            <SliderBackMain>
-                <SliderTest>
-                    다른 사람들의 3대 능력을
-                    <br /> 확인해보세요
-                </SliderTest>
-                <SliderContent>
-                    <SliderMain data={data} />
-                </SliderContent>
-            </SliderBackMain>
-        </AdverBackground>
-    );
+  return (
+    <AdverBackground>
+      <SliderBackMain>
+        <SliderTest>
+          다른 사람들의 3대 능력을
+          <br /> 확인해보세요
+        </SliderTest>
+        {/* <SliderContent>
+                    <SliderMain data={data} /> 
+                </SliderContent> */}
+      </SliderBackMain>
+    </AdverBackground>
+  );
 }
 
 const AdverBackground = styled.div`
-    width: 1440px;
-    height: 266px;
-    padding: 22px 12px;
+  width: 1440px;
+  height: 266px;
+  padding: 22px 12px;
 `;
 
 const SliderBackMain = styled.div`
-    position: relative;
-    width: 100%;
-    height: 100%;
-    background: linear-gradient(to left, #60c6ff, #1dc895);
-    border-radius: 8px;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to left, #60c6ff, #1dc895);
+  border-radius: 8px;
 `;
 
 const SliderTest = styled.span`
-    position: absolute;
-    width: 534px;
-    height: 88px;
-    left: 36px;
-    top: 138px;
+  position: absolute;
+  width: 534px;
+  height: 88px;
+  left: 36px;
+  top: 138px;
 
-    /* Tilte2/01_Bold */
+  /* Tilte2/01_Bold */
 
-    font-family: "Pretendard";
-    font-style: normal;
-    font-weight: 700;
-    font-size: 36px;
-    line-height: 44px;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 44px;
 
-    color: #ffffff;
+  color: #ffffff;
 `;
 
 const SliderContent = styled.div`
-    position: absolute;
-    top: 20px;
-    right: 54px;
-    width: 530px;
-    height: 226px;
-    background-color: #ff81df;
+  position: absolute;
+  top: 20px;
+  right: 54px;
+  width: 530px;
+  height: 226px;
+  background-color: #ff81df;
 `;
