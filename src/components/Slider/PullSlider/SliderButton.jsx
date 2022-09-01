0570442597eaeff323bda73f9cbd3a465b23b12f
pasong0511 <pasong0511@gmail.com>
2022-09-01 21:35:12 +0900

    Feat : 삼대력 게시판 상단 광고 슬라이드 추가

--- src/components/Slider/PullSlider/SliderButton.jsx
+++ src/components/Slider/PullSlider/SliderButton.jsx
@@ -3,32 +3,51 @@ import styled, { css } from "styled-components";
 import ArrowIcon from "../../../assets/images/board_prev.svg";
 
 const BtnSlideControl = styled.button`
-    position: absolute;
-    top: calc(50% - 30px);
-    padding: 20px 4px;
-    z-index: 1;
-    background-color: white;
-    width: 30px;
-    height: 60px;
-    opacity: 0.5;
-    border-radius: 15px;
-    border: none;
+  position: absolute;
+  top: calc(50% - 30px);
+  z-index: 1;
+  background-color: white;
+  width: 30px;
+  height: 60px;
+  opacity: 0.5;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    ${(props) =>
-        props.direction
-            ? css`
-                  transform: rotate(180deg);
-                  left: 1rem;
-              `
-            : css`
-                  right: 1rem;
-              `};
+  ${(props) =>
+    props.direction
+      ? css`
+          transform: rotate(180deg);
+          left: 1rem;
+        `
+      : css`
+          right: 1rem;
+        `};
+
+  ${(props) =>
+    props.categories === "home"
+      ? css`
+          padding: 20px 4px;
+          height: 60px;
+          border-radius: 15px;
+        `
+      : css`
+          padding: 0;
+          height: 30px;
+          border-radius: 50%;
+          top: 50%;
+        `};
 `;
 
-export default function SliderButton({ direction, onClick }) {
-    return (
-        <BtnSlideControl onClick={onClick} direction={direction ? 1 : 0}>
-            <img src={ArrowIcon} alt="슬라이드버튼"></img>
-        </BtnSlideControl>
-    );
+export default function SliderButton({ direction, onClick, categories }) {
+  return (
+    <BtnSlideControl
+      onClick={onClick}
+      direction={direction ? 1 : 0}
+      categories={categories}
+    >
+      <img src={ArrowIcon} alt="슬라이드버튼"></img>
+    </BtnSlideControl>
+  );
 }
