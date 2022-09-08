4d594bdca9a51f90f1260f3354685cf4af8cd54d
pasong0511 <pasong0511@gmail.com>
2022-09-08 21:11:42 +0900

    Fix : #130 3대력차트 오른쪽 이동 구현 완료

--- src/styles/Layout/Layout.jsx
+++ src/styles/Layout/Layout.jsx
@@ -15,10 +15,15 @@ export const CommonContainer = styled.div`
   position: relative;
 `;
 
+/*홈화면 2단 나누기*/
 export const CommonContentsHome = styled.div`
-  display: flex; /*홈화면 2단 나누기*/
+  width: 100%;
+  max-width: 1440px;
+  display: flex;
   margin: 0 auto;
-  @media screen and (max-width: 1500px) {
+  box-sizing: border-box;
+
+  @media screen and (max-width: 1440px) {
     flex-direction: column;
     align-items: center;
   }
@@ -29,39 +34,27 @@ export const CommonContentsHome = styled.div`
 `;
 
 /*홈화면 2단(왼쪽, 오른쪽 영역)*/
-
 /* 홈화면 오른쪽 인기사진, 게시판 목록 */
 export const CommonContentsLeft = styled.div`
-  width: 75%;
+  box-sizing: border-box;
   order: 1;
-  @media screen and (max-width: 1900px) {
-    width: 70%;
-    order: 1;
-  }
-  @media screen and (max-width: 1024px) {
-    width: 65%;
-    order: 1;
+  @media screen and (max-width: 1440px) {
+    order: 2;
   }
   @media screen and (max-width: 600px) {
-    width: 55%;
     order: 2;
   }
 `;
 
 /* 홈화면 왼쪽 삼대력 Top10 목록 */
 export const CommonContentsRight = styled.div`
-  width: 25%;
+  box-sizing: border-box;
   order: 2;
-  @media screen and (max-width: 1900px) {
-    width: 65%;
-    order: 2;
-  }
-  @media screen and (max-width: 1024px) {
-    width: 80%;
-    order: 2;
+  margin: 0 auto;
+  @media screen and (max-width: 1440px) {
+    order: 1;
   }
   @media screen and (max-width: 600px) {
-    width: 90%;
     order: 1;
   }
 `;
@@ -78,7 +71,7 @@ export const MainSlide = styled.div`
 /*메인화면 공통*/
 /*홈화면, 카테고리 컨텐츠 타이틀*/
 export const CommonTitleArea = styled.div`
-  width: 1100px;
+  /* max-width: 1100px; */
   margin: 30px auto;
 `;
 
@@ -97,29 +90,24 @@ export const CommonTitleText = styled.div`
 
 /*홈화면 개별 컴포넌트의 중앙 정렬 틀*/
 export const WidthAreaHome = styled.section`
-  width: 75%;
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 1039px;
   margin: 0 auto;
-  display: flex;
-  flex-wrap: wrap;
-  @media screen and (max-width: 1900px) {
-    width: 95%;
-  }
-  @media screen and (max-width: 1024px) {
-    width: 100%;
-  }
 `;
 
 /*(홈화면 제외)각 컴포넌트의 중앙 정렬 틀*/
 export const WidthAreaSpace = styled.section`
-  width: 1400px;
+  box-sizing: border-box;
+  width: 1396px;
   margin: 0 auto;
   margin-top: 100px;
-  @media screen and (max-width: 1400px) {
+  @media screen and (max-width: 1440px) {
     width: 700px;
     display: flex;
     flex-wrap: wrap;
   }
-  @media screen and (max-width: 700px) {
+  @media screen and (max-width: 600px) {
     width: 500px;
     display: flex;
     flex-wrap: wrap;
@@ -128,7 +116,8 @@ export const WidthAreaSpace = styled.section`
 
 /*(홈화면 제외) 게시글 컴포넌트의 중앙 정렬 틀*/
 export const WidthAreaInner = styled.div`
-  margin: 32px 2px;
+  box-sizing: border-box;
+  margin: 32px 0px;
   display: flex;
   flex-wrap: wrap;
 `;
