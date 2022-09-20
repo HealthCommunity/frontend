2a6869a4fc31833111188e33fe291f486b0ebdd5
pasong0511 <pasong0511@gmail.com>
2022-09-21 02:26:19 +0900

    refactor : 슬라이더 컴포넌트 주석 추가 및 변수 이름 변경

--- src/components/Board/BoardFetchItems.jsx
+++ src/components/Board/BoardFetchItems.jsx
@@ -25,21 +25,26 @@ export default function BoardFetchItems({ category = "", center }) {
   const [page, setPage] = useState(0); //현재 페이지
   const target = useRef();
   const [isLoading, setIsLoading] = useState(false);
+
+  //옵저버 감시되어 타겟이 탐지되면 실행되는 함수
   const onIntersect = async ([entry], observer) => {
+    //옵저버 감시영역에 들어옴
     if (entry.isIntersecting) {
-      observer.unobserve(entry.target);
+      observer.unobserve(entry.target); //게시글 목록 맨 아래 타겟팅
       setPage((page) => page + 1); //페이지 증가
     }
   };
 
   let categoryPath = "";
 
+  //내게시글 목록은 뒤에 경로가 없어서 별도로 추가 경로설정 필요
   if (category === "user") {
     categoryPath = `${category}/post`;
   } else {
     categoryPath = `${category}`;
   }
 
+  //옵저버로 증가시킨 pase 상태 적용해서 게시글 목록 api 요청
   const getRequest = async (page) => {
     setIsLoading(true);
     return axios
@@ -48,18 +53,20 @@ export default function BoardFetchItems({ category = "", center }) {
       )
       .then(({ data }) => data.data);
   };
+
   useEffect(() => {
     const observer = new IntersectionObserver(onIntersect, {
       threshold: 0.5,
     });
     getRequest(page).then((items) => {
+      //서버에서 받아온 api데이터가 0보다 작은 경우에는 옵저버 실행시키지 않는다
       if (items.length > 0) {
-        setItemList((prevItems) => [...prevItems, ...items]);
+        setItemList((prevItems) => [...prevItems, ...items]); //api로 받아온 게시글 목록 state에 데이터들 추가
         observer.observe(target.current); // 타겟 엘리먼트 지정
       }
     });
     return () => {
-      observer.disconnect();
+      observer.disconnect(); //옵저버 제거
     };
   }, [page, category]);
 
